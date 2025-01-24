cb9b8e137412dac37a38321ac18afbf2e807950e
Elif Yıldırım <elifyildirim3@outlook.com>
2025-01-24 22:03:25 +0300

    Add project files.

---
+++ Infrastructure/OnionNight.Persistence/Context/OnionContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnionNight.Domain.Entities;
+
+namespace OnionNight.Persistence.Context;
+public class OnionContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server = ;initial Catalog=OnionNightDb; integrated security=true");
+    }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+}
+
